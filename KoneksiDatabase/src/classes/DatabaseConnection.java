b7d346fad3b0b57117373dbb46edaa8f0768ab78
hafizh faikar <you@example.com>
2021-06-16 11:21:29 +0700

    Create Database dan Show Data

---
+++ KoneksiDatabase/src/classes/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package classes;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+public class DatabaseConnection {
+    public static Connection getKoneksi(String host, String port, String username, String password, String db) {
+        String connection = "jdbc:mysql://" + host + ":" + port + "/" + db;
+        Connection koneksi = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            getConnect = DriverManager.getConnection(connection,
+            username, password);
+            System.out.println("Koneksi Berhasil");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Koneksi Database Error");
+            getConnect = null;
+        }
+        return getConnect;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Hafizh_Faikar-XIRPL1-15
