4b382d6224fbbb3e36586fd8e718b9fb46cb789a
hafizh faikar <you@example.com>
2021-06-16 14:00:48 +0700

    Edit Data

--- KoneksiDatabase/src/form/DataSiswa.java
+++ KoneksiDatabase/src/form/DataSiswa.java
@@ -12,13 +12,13 @@ import javax.swing.table.DefaultTableModel;
 public class DataSiswa extends javax.swing.JFrame {
 
     Connection koneksi;
-
+    
     public DataSiswa() {
         initComponents();
         koneksi = DatabaseConnection.getKoneksi("localhost", "3306", "root", "", "db_sekolah");
         showData();
     }
-
+    
     DefaultTableModel dtm;
     public void showData(){
         String[] kolom = {"NO", "NIS", "Nama", "Kelas", "Jurusan"};
@@ -43,5 +43,181 @@ public class DataSiswa extends javax.swing.JFrame {
         }
         tbl_siswa.setModel(dtm);
     }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbl_siswa = new javax.swing.JTable();
+        cmdRefresh = new javax.swing.JButton();
+        cmdTambah = new javax.swing.JButton();
+        cmdEdit = new javax.swing.JButton();
+        cmdHapus = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("DATA SISWA");
+
+        tbl_siswa.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"", "", null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        tbl_siswa.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tbl_siswaMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tbl_siswa);
+
+        cmdRefresh.setText("Refresh");
+        cmdRefresh.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdRefreshActionPerformed(evt);
+            }
+        });
+
+        cmdTambah.setText("Tambah");
+        cmdTambah.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdTambahActionPerformed(evt);
+            }
+        });
+
+        cmdEdit.setText("Ubah");
+        cmdEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdEditActionPerformed(evt);
+            }
+        });
+
+        cmdHapus.setText("Hapus");
+        cmdHapus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdHapusActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(171, 171, 171))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(cmdRefresh)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(cmdTambah)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmdEdit)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmdHapus)))
+                .addContainerGap(15, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(7, 7, 7)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmdRefresh)
+                    .addComponent(cmdTambah)
+                    .addComponent(cmdEdit)
+                    .addComponent(cmdHapus))
+                .addContainerGap())
+        );
+
+        pack();
+    }
+
+    private void cmdRefreshActionPerformed(java.awt.event.ActionEvent evt) {
+    }
+
+    private void cmdEditActionPerformed(java.awt.event.ActionEvent evt) {
+        String nis = tbl_siswa.getValueAt(baris, 1).toString();
+        ManageData tambahData = new ManageData (this, true, "Edit", nis);
+        tambahData.setVisible(true);
+    }
+
+    private void cmdTambahActionPerformed(java.awt.event.ActionEvent evt) {
+        ManageData tambahData = new ManageData (this, true, "Tambah", "");
+        tambahData.setVisible(true);
+    }
+    
+    int baris;    
+    private void tbl_siswaMouseClicked(java.awt.event.MouseEvent evt) {
+        baris = tbl_siswa.getSelectedRow();
+    }
+      
+    private void cmdHapusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdHapusActionPerformed
+        String idWhoWantToBeDelete = tbl_siswa.getValueAt(baris, 0).toString();
+        try{
+            // TODO add your handling code here:
+            Statement stmt = koneksi.createStatement();
+            String query = "DELETE FROM t_siswa WHERE nis = '"+idWhoWantToBeDelete+"';";
+            int berhasil = stmt.executeUpdate(query);
+            if(berhasil == 1){
+                JOptionPane.showMessageDialog(null, " Data Berhasil Dihapus");
+                dtm.getDataVector().removeAllElements();
+                showData();
+            }else{
+                JOptionPane.showMessageDialog(null, " Data Gagal Dihapus");
+            }
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        
+    }
+
+    public static void main(String args[]) {
+     
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DataSiswa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DataSiswa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DataSiswa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DataSiswa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DataSiswa().setVisible(true);
+            }
+        });
+    }
+
+    private javax.swing.JButton cmdEdit;
+    private javax.swing.JButton cmdHapus;
+    private javax.swing.JButton cmdRefresh;
+    private javax.swing.JButton cmdTambah;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tbl_siswa;
 }
 
--- KoneksiDatabase/src/form/ManageData.java
+++ KoneksiDatabase/src/form/ManageData.java
@@ -10,11 +10,17 @@ import javax.swing.JOptionPane;
 public class ManageData extends javax.swing.JDialog {
 
     Connection koneksi;
-
+    String action;
     public ManageData(java.awt.Frame parent, boolean modal, String act, String nis) {
         super(parent, modal);
         initComponents();
         koneksi = DatabaseConnection.getKoneksi("localhost", "3306", "root", "", "db_sekolah");
+
+        action = act;
+        if(action.equals("Edit")){
+            txtNIS.setEnabled(false);
+            showData(nis);
+        }
     }
 
     public void SimpanData(){
@@ -39,39 +45,194 @@ public class ManageData extends javax.swing.JDialog {
         }
     }
 
-    private void btnSimpanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSimpanActionPerformed
-        // TODO add your handling code here:
-        SimpanData();
-    }
-
-    private void cmdTambahActionPerformed(java.awt.event.ActionEvent evt){
-        ManageData tambahData = new ManageData(this, true);
-        tambahData.setVisible(true)
-    }
-
-    int baris;
-    private void tbl_siswaMouseClicked(java.awt.event.ActionEvent evt){
-        baris = tbl_siswa.getSelectedRow();
-    }
-
-    private void cmdHapusActionPerformed(java.awt.event.ActionEvent evt){
-        String idWhoWantToDelete = tbl_siswa.getValueAt(baris, 0).toString();
-        try {
+    void showData(String nis){
+        try{
             Statement stmt = koneksi.createStatement();
-            String query = "DELETE from t_siswa where nis = "+idWhoWantToDelete+";";
-            int berhasil = stmt.executeUpdate(qurey);
+            String query = "SELECT * FROM t_siswa WHERE nis = '"+nis+"'";
+            ResultSet rs = stmt.executeQuery(query);
+            rs.first();
+            txtNIS.setText(rs.getString("nis"));
+            txtNama.setText(rs.getString("nama"));
+            cmbKelas.setSelectedItem(rs.getString("kelas"));
+            cmbJurusan.setSelectedItem(rs.getString("jurusan"));
+        }catch(SQLException ex){
+        ex.printStackTrace();
+        JOptionPane.showMessageDialog(null, "Terjadi Kesalahan di Query");
+        }
+    }
+    public void EditData(){
+        String nis      = txtNIS.getText();
+        String nama     = txtNama.getText();
+        String kelas    = cmbKelas.getSelectedItem().toString();
+        String jurusan  = cmbJurusan.getSelectedItem().toString();
+    
+        try{
+            Statement stmt = koneksi.createStatement();
+            String query = "UPDATE t_siswa SET nama = '"+nama+"'," + "kelas= '"+kelas+"'," + "jurusan= '"+jurusan+"' WHERE nis = '"+nis+"'";
+            System.out.println(query);
+            int berhasil = stmt.executeUpdate(query);
             if(berhasil == 1){
-                JOptionPane.showMessageDialog(null, "Data berhasil dihapus");
-                dtm.getDataVector().removeAllElements();
-                showData();
-            } else {
-                JOptionPane.showMessageDialog(null, "Data tidak berhasil di hapus")
+                JOptionPane.showMessageDialog(null, " Data Berhasil Diubah");
+            }else{
+                JOptionPane.showMessageDialog(null, " Data Gagal Diubah");
             }
-        } catch(SQLException e){
-            e.printStackTrace();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Terjadi Kesalahan Pada Query");
         }
     }
 
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lblTitle = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel2 = new javax.swing.JLabel();
+        txtNama = new javax.swing.JTextField();
+        cmbKelas = new javax.swing.JComboBox<>();
+        cmbJurusan = new javax.swing.JComboBox<>();
+        btnSimpan = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtNIS = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        lblTitle.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        lblTitle.setText("Tambah Data");
+
+        jLabel2.setText("NIS");
+
+        txtNama.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNamaActionPerformed(evt);
+            }
+        });
+
+        cmbKelas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "XI-RPL1", "XI-RPL2", "XI-RPL3", "XI-RPL4", "XI-RPL5" }));
+
+        cmbJurusan.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Rekayasa Perangkat Lunak", "Teknik Komputer dan Jaringan", "Multimedia", "Teknik Audio Vidio", "Teknik Otomasi Industri" }));
+        cmbJurusan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmbJurusanActionPerformed(evt);
+            }
+        });
+
+        btnSimpan.setText("Simpan");
+        btnSimpan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSimpanActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Nama Lengkap");
+
+        jLabel4.setText("Kelas");
+
+        jLabel5.setText("Jurusan");
+
+        txtNIS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNISActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jSeparator1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblTitle)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(btnSimpan)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(jLabel4)
+                                            .addComponent(jLabel3)
+                                            .addComponent(jLabel5))
+                                        .addGap(40, 40, 40)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(cmbJurusan, 0, 223, Short.MAX_VALUE)
+                                            .addComponent(cmbKelas, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(txtNama)
+                                            .addComponent(txtNIS, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                        .addGap(0, 29, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(lblTitle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txtNIS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNama, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmbKelas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmbJurusan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addGap(18, 18, 18)
+                .addComponent(btnSimpan)
+                .addGap(30, 30, 30))
+        );
+
+        pack();
+    }
+
+    public static void main(String args[]){
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run(){
+                ManageData dialog = new ManageData(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindwAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        })
+    }
+
+    private void btnSimpanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSimpanActionPerformed
+        // TODO add your handling code here:
+        if(action.equals("Edit")) EditData();
+        SimpanData();
+    }
+
+    private javax.swing.JButton btnSimpan;
+    private javax.swing.JComboBox<String> cmbJurusan;
+    private javax.swing.JComboBox<String> cmbKelas;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JTextField txtNIS;
+    private javax.swing.JTextField txtNama;
 
 }
 
